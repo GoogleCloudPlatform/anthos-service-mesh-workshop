f251c960995e2b1a6d69537b905df3e95ba68289
terraform <ameerabbas@google.com>
2019-12-19 20:18:24 -0800

    created folder in setup script

--- infrastructure/network/prod/host_project/outputs.tf
+++ infrastructure/network/prod/host_project/outputs.tf
@@ -1,3 +1,2 @@
 # project ids
-    output "host_project_id" { value = "${module.create_host_project.project_id}" }
-    output "folder_name" { value = "${google_folder.folder.name}"}
+output "host_project_id" { value = "${module.create_host_project.project_id}" }
