b64586088737e3b6d45ae39a7a6acf4caa499840
Ameer Abbas <ameer00@gmail.com>
2019-12-21 12:32:40 -0800

    Update setup-terraform-admin-project.sh

--- scripts/setup-terraform-admin-project.sh
+++ scripts/setup-terraform-admin-project.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # Copyright 2019 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,8 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-#!/usr/bin/env bash
 
 SCRIPT_DIR=$(dirname $(readlink -f $0 2>/dev/null) 2>/dev/null || echo "${PWD}/$(dirname $0)")
 
