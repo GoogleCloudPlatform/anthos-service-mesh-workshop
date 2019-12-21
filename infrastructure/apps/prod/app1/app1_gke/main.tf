e43351a1d95925384c38a5d4a1da86515c80f6bc
Ameer Abbas <ameer00@gmail.com>
2019-12-21 12:23:58 -0800

    Update main.tf

--- infrastructure/apps/prod/app1/app1_gke/main.tf
+++ infrastructure/apps/prod/app1/app1_gke/main.tf
@@ -1,3 +1,17 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # gke-1-apps-r1a-prod - Create GKE zonal cluster in dev1 project using subnet-03 zone a
 module "create_gke_1_dev1_r1a_subnet_03" {
   source  = "terraform-google-modules/kubernetes-engine/google"
