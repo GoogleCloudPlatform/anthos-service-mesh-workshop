1bb2fe0d3a8f6e6c0b8ab6109fa69b47900e52d5
Megan O'Keefe <megan037@gmail.com>
2020-01-13 17:40:13 -0500

    Fix

--- k8s_manifests/prod/app-canary/auto-canary.sh
+++ k8s_manifests/prod/app-canary/auto-canary.sh
@@ -7,7 +7,7 @@ log() { echo "$1" >&2; }
 
 
 active_v2_weight () {
- w=`kubectl --context=${OPS_CLUSTER} get virtualservice -n frontend frontend -o json \
+ w=`kubectl --context=${OPS_CONTEXT} get virtualservice -n frontend frontend -o json \
  | jq -r '.spec.http[].route[]|select(.destination.subset == "v2") .weight'`
 
  log "v2 deployed weight is ${w}%"
@@ -18,10 +18,10 @@ active_v2_weight () {
 run_canary() {
     PERCENT=$1
     # copy all manifests in canary-${PERCENT}/  to k8s_repo
-    cp canary-${PERCENT}/*  ../k8s_repo/${OPS_CLUSTER}/app-canary/
+    cp canary-${PERCENT}/*  ../k8s_repo/${OPS_DIR}/app-canary/
 
     # commit to K8s_repo
-    cd ../k8s_repo/${OPS_CLUSTER}/
+    cd ../k8s_repo/${OPS_DIR}/
     git add .
     git commit -m "Canary deployment - ${PERCENT}% to frontend-v2"
     git push origin master
@@ -37,15 +37,23 @@ run_canary() {
 
 
 # make sure ops cluster is set
-if [ -z "$OPS_CLUSTER" ]
+if [ -z "$OPS_DIR" ]
 then
-    log "You must set OPS_CLUSTER to continue."
+    log "You must set OPS_DIR to continue."
     exit
 else
-    PROJECT_ID=$OPS_CLUSTER
+    OPS_DIR=$OPS_DIR
 fi
 
-log "🐤 Starting frontend canary, OPS_CLUSTER=${OPS_CLUSTER}"
+if [ -z "$OPS_CONTEXT" ]
+then
+    log "You must set OPS_CONTEXT to continue."
+    exit
+else
+    OPS_CONTEXT=$OPS_CONTEXT
+fi
+
+log "🐤 Starting frontend canary, OPS_DIR=${OPS_DIR}"
 declare -a percentages=("20" "50" "80" "100")
 
 for i in "${percentages[@]}"
