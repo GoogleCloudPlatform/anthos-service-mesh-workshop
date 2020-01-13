8d0ea198c30224011cc4213a4ba857c1f2998432
Megan O'Keefe <megan037@gmail.com>
2020-01-13 18:22:39 -0500

    script fixes

--- k8s_manifests/prod/app-canary/auto-canary.sh
+++ k8s_manifests/prod/app-canary/auto-canary.sh
@@ -18,7 +18,7 @@ active_v2_percent () {
 run_canary() {
     PERCENT=$1
     # copy all manifests in canary-${PERCENT}/  to k8s_repo
-    cp canary-${PERCENT}/*  ../k8s_repo/${OPS_DIR}/app-canary/
+    cp canary${PERCENT}/*  ../k8s_repo/${OPS_DIR}/app-canary/
 
     # commit to K8s_repo
     cd ../k8s_repo/${OPS_DIR}/
