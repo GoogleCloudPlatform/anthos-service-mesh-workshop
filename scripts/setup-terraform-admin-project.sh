e434ac504de45e9a8acb3defb88ed47f8c152d22
terraform <273651294571@cloudbuild.gserviceaccount.com>
2020-01-16 12:48:52 -0800

    update suffix

--- scripts/setup-terraform-admin-project.sh
+++ scripts/setup-terraform-admin-project.sh
@@ -73,10 +73,10 @@ ORG_USER=${MY_USER?env not set}
 ORG_USER=${MY_USER%@*}
 ORG_USER=${ORG_USER:0:11}
 export TF_VAR_folder_display_name=${ORG_USER}-${RANDOM_PERSIST}-asm
-PROJECT_ID_SUFFIX=$(od -An -N4 -D /dev/random)
-PROJECT_ID_SUFFIX=${PROJECT_ID_SUFFIX:0:5}
+PROJECT_ID_SUFFIX=$(od -An -N4 -D /dev/random | tr -d " ")
+PROJECT_ID_SUFFIX=${PROJECT_ID_SUFFIX:1:5}
 export TF_ADMIN_NAME=${ORG_USER}-${RANDOM_PERSIST}-tf
-export TF_ADMIN=${ORG_USER}-${RANDOM_PERSIST}-tf-${PROJECT_ID_SUFFIX}
+export TF_ADMIN=${TF_ADMIN_NAME}-tf-${PROJECT_ID_SUFFIX}
 
 echo -e "\n${CYAN}Checking for existing asm workshop folder...${NC}" 
 export TF_VAR_folder_id=$(gcloud resource-manager folders list --organization=${TF_VAR_org_id} | grep ${TF_VAR_folder_display_name} | awk '{print $3}')
