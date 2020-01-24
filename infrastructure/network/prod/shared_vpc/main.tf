154751d72f99af8d8415e30ed8dfc7df8dce8900
Megan O'Keefe <megan037@gmail.com>
2020-01-23 20:04:59 -0500

    rename

--- infrastructure/network/prod/shared_vpc/main.tf
+++ infrastructure/network/prod/shared_vpc/main.tf
@@ -127,7 +127,7 @@ module "net-firewall" {
 }
 
 # create firewall rule - allow ssh / scp from all
-resource "google_compute_firewall" "default" {
+resource "google_compute_firewall" "ssh-scp-all" {
   name    = "ssh-scp-all"
   network = module.create_vpc_in_host_project.network_name
   allow {
