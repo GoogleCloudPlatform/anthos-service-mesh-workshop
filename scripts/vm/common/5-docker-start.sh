856b10b3f9cc8b2a4e80a54cfb9f80df78c22f4f
Megan O'Keefe <megan037@gmail.com>
2020-01-24 14:21:30 -0500

    updates

--- scripts/vm/common/5-docker-start.sh
+++ scripts/vm/common/5-docker-start.sh
@@ -6,6 +6,6 @@ log "🐳 Starting ${SVC_NAME} on ${VM_NAME}..."
 
 DOCKER_CMD="sudo docker run -d --name $SVC_NAME -p $SVC_PORT:$SVC_PORT -e "PORT=$SVC_PORT" $IMAGE"
 
-gcloud compute ssh --project ${TF_VAR_dev1_project_name} --zone $VM_ZONE \ -- "${DOCKER_CMD}"
+gcloud compute ssh --project ${TF_VAR_dev1_project_name} --zone $VM_ZONE --command "${DOCKER_CMD}"
 
 log "✅ Done."
