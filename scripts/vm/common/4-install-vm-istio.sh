7f9e8930d4f5b4ae629874a712c25c188fa032fe
Megan O'Keefe <megan037@gmail.com>
2020-01-24 14:26:08 -0500

    updates

--- scripts/vm/common/4-install-vm-istio.sh
+++ scripts/vm/common/4-install-vm-istio.sh
@@ -7,6 +7,6 @@ log "🛸 Installing Istio on the VM..."
 GWIP=$(kubectl --context ${OPS_GKE_1} get -n istio-system service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 echo "Ops1 Ingress Gateway IP is ${GWIP}"
 
-gcloud compute ssh $VM_NAME --project ${TF_VAR_dev1_project_name} --zone $VM_ZONE -- "ISTIO_VERSION='1.4.2' GWIP=${GWIP} ./run-on-vm.sh"
+gcloud compute ssh $VM_NAME --project ${TF_VAR_dev1_project_name} --zone $VM_ZONE -- "ISTIO_VERSION='1.4.3' GWIP=${GWIP} ./run-on-vm.sh"
 
 log "✅ Done."
