e7ef08e98775d6a42a90bf1626f0e03d0d845cf8
Megan O'Keefe <megan037@gmail.com>
2020-01-24 13:33:22 -0500

    updates

--- scripts/vm/common/3-update-repo.sh
+++ scripts/vm/common/3-update-repo.sh
@@ -18,10 +18,10 @@ source ../${1}/env.sh
 
 
 # log "⭐️ Generating ServiceEntry.."
-# sed -i -e "s/[[SVC_NAME]]/${SVC_NAME}/g" -e "s/[[SVC_PORT]]/${SVC_PORT}/g" -e "s/[[SVC_NAMESPACE]]/${SVC_NAMESPACE}/g" -e "s/[[GCE_IP]]/${GCE_IP}/g" \
+# sed -i -e "s/{SVC_NAME}/${SVC_NAME}/g" -e "s/{SVC_PORT}/${SVC_PORT}/g" -e "s/{SVC_NAMESPACE}/${SVC_NAMESPACE}/g" -e "s/{GCE_IP}/${GCE_IP}/g" \
 # service-entry.tpl.yaml > ${K8S_REPO}/${OPS_GKE_1_CLUSTER}/istio-networking/${FILE_NAME}-service-entry.yaml
 
-# sed -i -e "s/[[SVC_NAME]]/${SVC_NAME}/g" -e "s/[[SVC_PORT]]/${SVC_PORT}/g" -e "s/[[SVC_NAMESPACE]]/${SVC_NAMESPACE}/g" -e "s/[[GCE_IP]]/${GCE_IP}/g" \
+# sed -i -e "s/{SVC_NAME}/${SVC_NAME}/g" -e "s/{SVC_PORT}/${SVC_PORT}/g" -e "s/{SVC_NAMESPACE}/${SVC_NAMESPACE}/g" -e "s/{GCE_IP}/${GCE_IP}/g" \
 # service-entry.tpl.yaml > ${K8S_REPO}/${OPS_GKE_2_CLUSTER}/istio-networking/${FILE_NAME}-service-entry.yaml
 
 # echo "  - ${FILE_NAME}-service-entry.yaml" >> ${K8S_REPO}/${OPS_GKE_1_CLUSTER}/istio-networking/kustomization.yaml
@@ -29,22 +29,22 @@ source ../${1}/env.sh
 
 
 log "☸️ Generating selector-less Kubernetes Service.."
-sed -e "s/[[SVC_NAME]]/$SVC_NAME/g" -e "s/[[SVC_PORT]]/$SVC_PORT/g" -e "s/[[SVC_NAMESPACE]]/$SVC_NAMESPACE/g"  \
+sed -e "s/{SVC_NAME}/$SVC_NAME/g" -e "s/{SVC_PORT}/$SVC_PORT/g" -e "s/{SVC_NAMESPACE}/$SVC_NAMESPACE/g"  \
 service.tpl.yaml > ${K8S_REPO}/${OPS_GKE_1_CLUSTER}/app/services/${FILE_NAME}-svc.yaml
 
-# sed -i -e "s/[[SVC_NAME]]/${SVC_NAME}/g" -e "s/[[SVC_PORT]]/${SVC_PORT}/g" -e "s/[[SVC_NAMESPACE]]/${SVC_NAMESPACE}/g"  \
+# sed -i -e "s/{SVC_NAME}/${SVC_NAME}/g" -e "s/{SVC_PORT}/${SVC_PORT}/g" -e "s/{SVC_NAMESPACE}/${SVC_NAMESPACE}/g"  \
 # service.tpl.yaml > ${K8S_REPO}/${OPS_GKE_1_CLUSTER}/app/services/${FILE_NAME}-svc.yaml
 
-# sed -i -e "s/[[SVC_NAME]]/${SVC_NAME}/g" -e "s/[[SVC_PORT]]/${SVC_PORT}/g" -e "s/[[SVC_NAMESPACE]]/${SVC_NAMESPACE}/g"  \
+# sed -i -e "s/{SVC_NAME}/${SVC_NAME}/g" -e "s/{SVC_PORT}/${SVC_PORT}/g" -e "s/{SVC_NAMESPACE}/${SVC_NAMESPACE}/g"  \
 # service.tpl.yaml > ${K8S_REPO}/${DEV1_GKE_1_CLUSTER}/app/services/${FILE_NAME}-svc.yaml
 
-# sed -i -e "s/[[SVC_NAME]]/${SVC_NAME}/g" -e "s/[[SVC_PORT]]/${SVC_PORT}/g" -e "s/[[SVC_NAMESPACE]]/${SVC_NAMESPACE}/g"  \
+# sed -i -e "s/{SVC_NAME}/${SVC_NAME}/g" -e "s/{SVC_PORT}/${SVC_PORT}/g" -e "s/{SVC_NAMESPACE}/${SVC_NAMESPACE}/g"  \
 # service.tpl.yaml > ${K8S_REPO}/${DEV1_GKE_2_CLUSTER}/app/services/${FILE_NAME}-svc.yaml
 
-# sed -i -e "s/[[SVC_NAME]]/${SVC_NAME}/g" -e "s/[[SVC_PORT]]/${SVC_PORT}/g" -e "s/[[SVC_NAMESPACE]]/${SVC_NAMESPACE}/g"  \
+# sed -i -e "s/{SVC_NAME}/${SVC_NAME}/g" -e "s/{SVC_PORT}/${SVC_PORT}/g" -e "s/{SVC_NAMESPACE}/${SVC_NAMESPACE}/g"  \
 # service.tpl.yaml > ${K8S_REPO}/${DEV2_GKE_1_CLUSTER}/app/services/${FILE_NAME}-svc.yaml
 
-# sed -i -e "s/[[SVC_NAME]]/${SVC_NAME}/g" -e "s/[[SVC_PORT]]/${SVC_PORT}/g" -e "s/[[SVC_NAMESPACE]]/${SVC_NAMESPACE}/g"  \
+# sed -i -e "s/{SVC_NAME}/${SVC_NAME}/g" -e "s/{SVC_PORT}/${SVC_PORT}/g" -e "s/{SVC_NAMESPACE}/${SVC_NAMESPACE}/g"  \
 # service.tpl.yaml > ${K8S_REPO}/${DEV2_GKE_2_CLUSTER}/app/services/${FILE_NAME}-svc.yaml
 
 # Push to repo
