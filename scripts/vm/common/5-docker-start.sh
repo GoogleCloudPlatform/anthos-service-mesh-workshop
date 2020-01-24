931187ab8c6fd59e7aa076d0a77295d3f1f1aaac
Megan O'Keefe <megan037@gmail.com>
2020-01-24 14:23:12 -0500

    updates

--- scripts/vm/common/5-docker-start.sh
+++ scripts/vm/common/5-docker-start.sh
@@ -4,7 +4,7 @@ source ../${1}/env.sh
 
 log "🐳 Starting ${SVC_NAME} on ${VM_NAME}..."
 
-DOCKER_CMD="sudo docker run -d --name $SVC_NAME -p $SVC_PORT:$SVC_PORT -e "PORT=$SVC_PORT" $IMAGE"
+DOCKER_CMD="sudo docker run -d --name $SVC_NAME -p $SVC_PORT:$SVC_PORT -e 'PORT=$SVC_PORT' $IMAGE"
 echo ${DOCKER_CMD}
 gcloud compute ssh ${VM_NAME} --project ${TF_VAR_dev1_project_name} --zone $VM_ZONE --command "${DOCKER_CMD}"
 
