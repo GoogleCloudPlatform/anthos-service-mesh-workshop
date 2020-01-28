5fafb7421c1fb7486c0999833392658c9e428098
Megan O'Keefe <megan037@gmail.com>
2020-01-28 10:07:01 -0500

    fix

--- k8s_manifests/prod/app-canary/repo-setup.sh
+++ k8s_manifests/prod/app-canary/repo-setup.sh
@@ -36,7 +36,7 @@ cp baseline/app-respy.yaml ${K8S_REPO}/${DEV2_GKE_1_CLUSTER}/app/deployments/
 echo "  - app-respy.yaml" >> ${K8S_REPO}/${DEV2_GKE_1_CLUSTER}/app/deployments/kustomization.yaml
 
 # VS and DR
-mkdir -p ${K8S_REPO}/${OPS_GKE_1_CLUSTER}/app-canary/
+mkdir -p ${K8S_REPO}/${OPS_GKE_2_CLUSTER}/app-canary/
 sed -i '/  - app-ingress\//a\ \ - app-canary\/' ${K8S_REPO}/${OPS_GKE_2_CLUSTER}/kustomization.yaml
 cp baseline/kustomization.yaml ${K8S_REPO}/${OPS_GKE_2_CLUSTER}/app-canary/
 cp baseline/dr-frontend.yaml ${K8S_REPO}/${OPS_GKE_2_CLUSTER}/app-canary/
