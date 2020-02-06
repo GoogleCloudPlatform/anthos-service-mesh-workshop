09f4c1c721f596070c26acdf152e1ce26932102d
Amina Mansour <aminamansour@gmail.com>
2020-02-06 01:51:18 -0800

    no more

--- fasttrack_scripts/deploy_application.sh
+++ fasttrack_scripts/deploy_application.sh
@@ -50,151 +50,151 @@ else
   echo "git repo already initialized."
 fi
 
-## Copy manifests to source repo
-#echo -e "\n"
-#echo "${bold}Copy the Hipster Shop namespaces and services to the source repo for all clusters${normal}"
-#read -p ''
-#print_and_execute "cd ${WORKDIR}/asm"
-#print_and_execute "cp -r k8s_manifests/prod/app/namespaces ../k8s-repo/${DEV1_GKE_1_CLUSTER}/app/"
-#print_and_execute "cp -r k8s_manifests/prod/app/namespaces ../k8s-repo/${DEV1_GKE_2_CLUSTER}/app/"
-#print_and_execute "cp -r k8s_manifests/prod/app/namespaces ../k8s-repo/${DEV2_GKE_1_CLUSTER}/app/"
-#print_and_execute "cp -r k8s_manifests/prod/app/namespaces ../k8s-repo/${DEV2_GKE_2_CLUSTER}/app/"
-#print_and_execute "cp -r k8s_manifests/prod/app/namespaces ../k8s-repo/${OPS_GKE_1_CLUSTER}/app/"
-#print_and_execute "cp -r k8s_manifests/prod/app/namespaces ../k8s-repo/${OPS_GKE_2_CLUSTER}/app/"
-#echo -e "\n"
-#print_and_execute "cp -r k8s_manifests/prod/app/services ../k8s-repo/${DEV1_GKE_1_CLUSTER}/app/"
-#print_and_execute "cp -r k8s_manifests/prod/app/services ../k8s-repo/${DEV1_GKE_2_CLUSTER}/app/"
-#print_and_execute "cp -r k8s_manifests/prod/app/services ../k8s-repo/${DEV2_GKE_1_CLUSTER}/app/"
-#print_and_execute "cp -r k8s_manifests/prod/app/services ../k8s-repo/${DEV2_GKE_2_CLUSTER}/app/"
-#print_and_execute "cp -r k8s_manifests/prod/app/services ../k8s-repo/${OPS_GKE_1_CLUSTER}/app/"
-#print_and_execute "cp -r k8s_manifests/prod/app/services ../k8s-repo/${OPS_GKE_2_CLUSTER}/app/"
-#
-#echo -e "\n"
-#echo "${bold}Copy the app directory kustomization.yaml to all clusters"
-#read -p ''
-#print_and_execute "cd ${WORKDIR}/asm"
-#print_and_execute "cp k8s_manifests/prod/app/kustomization.yaml ../k8s-repo/${DEV1_GKE_1_CLUSTER}/app/"
-#print_and_execute "cp k8s_manifests/prod/app/kustomization.yaml ../k8s-repo/${DEV1_GKE_2_CLUSTER}/app/"
-#print_and_execute "cp k8s_manifests/prod/app/kustomization.yaml ../k8s-repo/${DEV2_GKE_1_CLUSTER}/app/"
-#print_and_execute "cp k8s_manifests/prod/app/kustomization.yaml ../k8s-repo/${DEV2_GKE_2_CLUSTER}/app/"
-#print_and_execute "cp k8s_manifests/prod/app/kustomization.yaml ../k8s-repo/${OPS_GKE_1_CLUSTER}/app/"
-#print_and_execute "cp k8s_manifests/prod/app/kustomization.yaml ../k8s-repo/${OPS_GKE_2_CLUSTER}/app/"
+# Copy manifests to source repo
+echo -e "\n"
+echo "${bold}Copy the Hipster Shop namespaces and services to the source repo for all clusters${normal}"
+read -p ''
+print_and_execute "cd ${WORKDIR}/asm"
+print_and_execute "cp -r k8s_manifests/prod/app/namespaces ../k8s-repo/${DEV1_GKE_1_CLUSTER}/app/"
+print_and_execute "cp -r k8s_manifests/prod/app/namespaces ../k8s-repo/${DEV1_GKE_2_CLUSTER}/app/"
+print_and_execute "cp -r k8s_manifests/prod/app/namespaces ../k8s-repo/${DEV2_GKE_1_CLUSTER}/app/"
+print_and_execute "cp -r k8s_manifests/prod/app/namespaces ../k8s-repo/${DEV2_GKE_2_CLUSTER}/app/"
+print_and_execute "cp -r k8s_manifests/prod/app/namespaces ../k8s-repo/${OPS_GKE_1_CLUSTER}/app/"
+print_and_execute "cp -r k8s_manifests/prod/app/namespaces ../k8s-repo/${OPS_GKE_2_CLUSTER}/app/"
+echo -e "\n"
+print_and_execute "cp -r k8s_manifests/prod/app/services ../k8s-repo/${DEV1_GKE_1_CLUSTER}/app/"
+print_and_execute "cp -r k8s_manifests/prod/app/services ../k8s-repo/${DEV1_GKE_2_CLUSTER}/app/"
+print_and_execute "cp -r k8s_manifests/prod/app/services ../k8s-repo/${DEV2_GKE_1_CLUSTER}/app/"
+print_and_execute "cp -r k8s_manifests/prod/app/services ../k8s-repo/${DEV2_GKE_2_CLUSTER}/app/"
+print_and_execute "cp -r k8s_manifests/prod/app/services ../k8s-repo/${OPS_GKE_1_CLUSTER}/app/"
+print_and_execute "cp -r k8s_manifests/prod/app/services ../k8s-repo/${OPS_GKE_2_CLUSTER}/app/"
+
+echo -e "\n"
+echo "${bold}Copy the app directory kustomization.yaml to all clusters"
+read -p ''
+print_and_execute "cd ${WORKDIR}/asm"
+print_and_execute "cp k8s_manifests/prod/app/kustomization.yaml ../k8s-repo/${DEV1_GKE_1_CLUSTER}/app/"
+print_and_execute "cp k8s_manifests/prod/app/kustomization.yaml ../k8s-repo/${DEV1_GKE_2_CLUSTER}/app/"
+print_and_execute "cp k8s_manifests/prod/app/kustomization.yaml ../k8s-repo/${DEV2_GKE_1_CLUSTER}/app/"
+print_and_execute "cp k8s_manifests/prod/app/kustomization.yaml ../k8s-repo/${DEV2_GKE_2_CLUSTER}/app/"
+print_and_execute "cp k8s_manifests/prod/app/kustomization.yaml ../k8s-repo/${OPS_GKE_1_CLUSTER}/app/"
+print_and_execute "cp k8s_manifests/prod/app/kustomization.yaml ../k8s-repo/${OPS_GKE_2_CLUSTER}/app/"
 
 
-#echo -e "\n"
-#echo "${bold}Copy the Hipster Shop deployments, rbac and psp to the source repo for dev clusters${normal}"
-#read -p ''
-#print_and_execute "cp -r k8s_manifests/prod/app/deployments ../k8s-repo/${DEV1_GKE_1_CLUSTER}/app/"
-#print_and_execute "cp -r k8s_manifests/prod/app/deployments ../k8s-repo/${DEV1_GKE_2_CLUSTER}/app/"
-#print_and_execute "cp -r k8s_manifests/prod/app/deployments ../k8s-repo/${DEV2_GKE_1_CLUSTER}/app/"
-#print_and_execute "cp -r k8s_manifests/prod/app/deployments ../k8s-repo/${DEV2_GKE_2_CLUSTER}/app/"
-#echo -e "\n"
-#print_and_execute "cp -r k8s_manifests/prod/app/rbac ../k8s-repo/${DEV1_GKE_1_CLUSTER}/app/"
-#print_and_execute "cp -r k8s_manifests/prod/app/rbac ../k8s-repo/${DEV1_GKE_2_CLUSTER}/app/"
-#print_and_execute "cp -r k8s_manifests/prod/app/rbac ../k8s-repo/${DEV2_GKE_1_CLUSTER}/app/"
-#print_and_execute "cp -r k8s_manifests/prod/app/rbac ../k8s-repo/${DEV2_GKE_2_CLUSTER}/app/"
-#echo -e "\n"
-#print_and_execute "cp -r k8s_manifests/prod/app/podsecuritypolicies ../k8s-repo/${DEV1_GKE_1_CLUSTER}/app/"
-#print_and_execute "cp -r k8s_manifests/prod/app/podsecuritypolicies ../k8s-repo/${DEV1_GKE_2_CLUSTER}/app/"
-#print_and_execute "cp -r k8s_manifests/prod/app/podsecuritypolicies ../k8s-repo/${DEV2_GKE_1_CLUSTER}/app/"
-#print_and_execute "cp -r k8s_manifests/prod/app/podsecuritypolicies ../k8s-repo/${DEV2_GKE_2_CLUSTER}/app/"
-#
-#echo -e "\n"
-#echo "${bold}Remove podsecuritypolicies, deployments and rbac directories from ops clusters kustomization.yaml${normal}"
-#read -p ''
-#print_and_execute "sed -i -e '/- deployments\//d' -e '/- podsecuritypolicies\//d'  -e '/- rbac\//d' ../k8s-repo/${OPS_GKE_1_CLUSTER}/app/kustomization.yaml"
-#print_and_execute "sed -i -e '/- deployments\//d' -e '/- podsecuritypolicies\//d'  -e '/- rbac\//d' ../k8s-repo/${OPS_GKE_2_CLUSTER}/app/kustomization.yaml"
+echo -e "\n"
+echo "${bold}Copy the Hipster Shop deployments, rbac and psp to the source repo for dev clusters${normal}"
+read -p ''
+print_and_execute "cp -r k8s_manifests/prod/app/deployments ../k8s-repo/${DEV1_GKE_1_CLUSTER}/app/"
+print_and_execute "cp -r k8s_manifests/prod/app/deployments ../k8s-repo/${DEV1_GKE_2_CLUSTER}/app/"
+print_and_execute "cp -r k8s_manifests/prod/app/deployments ../k8s-repo/${DEV2_GKE_1_CLUSTER}/app/"
+print_and_execute "cp -r k8s_manifests/prod/app/deployments ../k8s-repo/${DEV2_GKE_2_CLUSTER}/app/"
+echo -e "\n"
+print_and_execute "cp -r k8s_manifests/prod/app/rbac ../k8s-repo/${DEV1_GKE_1_CLUSTER}/app/"
+print_and_execute "cp -r k8s_manifests/prod/app/rbac ../k8s-repo/${DEV1_GKE_2_CLUSTER}/app/"
+print_and_execute "cp -r k8s_manifests/prod/app/rbac ../k8s-repo/${DEV2_GKE_1_CLUSTER}/app/"
+print_and_execute "cp -r k8s_manifests/prod/app/rbac ../k8s-repo/${DEV2_GKE_2_CLUSTER}/app/"
+echo -e "\n"
+print_and_execute "cp -r k8s_manifests/prod/app/podsecuritypolicies ../k8s-repo/${DEV1_GKE_1_CLUSTER}/app/"
+print_and_execute "cp -r k8s_manifests/prod/app/podsecuritypolicies ../k8s-repo/${DEV1_GKE_2_CLUSTER}/app/"
+print_and_execute "cp -r k8s_manifests/prod/app/podsecuritypolicies ../k8s-repo/${DEV2_GKE_1_CLUSTER}/app/"
+print_and_execute "cp -r k8s_manifests/prod/app/podsecuritypolicies ../k8s-repo/${DEV2_GKE_2_CLUSTER}/app/"
 
-#echo -e "\n"
-#echo "${bold}Remove cartservice from all but one dev cluster ${normal}"
-#read -p ''
-#print_and_execute "rm ../k8s-repo/${DEV1_GKE_2_CLUSTER}/app/deployments/app-cart-service.yaml"
-#print_and_execute "rm ../k8s-repo/${DEV1_GKE_2_CLUSTER}/app/podsecuritypolicies/cart-psp.yaml"
-#print_and_execute "rm ../k8s-repo/${DEV1_GKE_2_CLUSTER}/app/rbac/cart-rbac.yaml"
-#echo -e "\n"
-#print_and_execute "rm ../k8s-repo/${DEV2_GKE_1_CLUSTER}/app/deployments/app-cart-service.yaml"
-#print_and_execute "rm ../k8s-repo/${DEV2_GKE_1_CLUSTER}/app/podsecuritypolicies/cart-psp.yaml"
-#print_and_execute "rm ../k8s-repo/${DEV2_GKE_1_CLUSTER}/app/rbac/cart-rbac.yaml"
-#echo -e "\n"
-#print_and_execute "rm ../k8s-repo/${DEV2_GKE_2_CLUSTER}/app/deployments/app-cart-service.yaml"
-#print_and_execute "rm ../k8s-repo/${DEV2_GKE_2_CLUSTER}/app/podsecuritypolicies/cart-psp.yaml"
-#print_and_execute "rm ../k8s-repo/${DEV2_GKE_2_CLUSTER}/app/rbac/cart-rbac.yaml"
-#
-#echo -e "\n"
-#echo "${bold}Add cartservice deployment, rbac and podsecuritypolicy to kustomization.yaml in the first dev cluster only${normal}"
-#read -p ''
-#print_and_execute "cd ../k8s-repo/${DEV1_GKE_1_CLUSTER}/app"
-#print_and_execute "cd deployments && kustomize edit add resource app-cart-service.yaml"
-#print_and_execute "cd ../podsecuritypolicies && kustomize edit add resource cart-psp.yaml"
-#print_and_execute "cd ../rbac && kustomize edit add resource cart-rbac.yaml"
-#print_and_execute "cd ${WORKDIR}/asm"
-#
-#echo -e "\n"
-#echo "${bold}Replace the PROJECT_ID in the RBAC manifests.${normal}"
-#read -p ''
-#print_and_execute "sed -i 's/\${PROJECT_ID}/'${TF_VAR_dev1_project_name}'/g'  ../k8s-repo/${DEV1_GKE_1_CLUSTER}/app/rbac/*"
-#print_and_execute "sed -i 's/\${PROJECT_ID}/'${TF_VAR_dev1_project_name}'/g'  ../k8s-repo/${DEV1_GKE_2_CLUSTER}/app/rbac/*"
-#print_and_execute "sed -i 's/\${PROJECT_ID}/'${TF_VAR_dev2_project_name}'/g'  ../k8s-repo/${DEV2_GKE_1_CLUSTER}/app/rbac/*"
-#print_and_execute "sed -i 's/\${PROJECT_ID}/'${TF_VAR_dev2_project_name}'/g'  ../k8s-repo/${DEV2_GKE_2_CLUSTER}/app/rbac/*"
-#
-#echo -e "\n"
-#echo "${bold}Copy the IngressGateway and VirtualService manifests to the source repo for the ops clusters.${normal}"
-#read -p ''
-#print_and_execute "cp -r k8s_manifests/prod/app-ingress/* ../k8s-repo/${OPS_GKE_1_CLUSTER}/app-ingress/"
-#print_and_execute "cp -r k8s_manifests/prod/app-ingress/* ../k8s-repo/${OPS_GKE_2_CLUSTER}/app-ingress/"
-#
-#echo -e "\n"
-#echo "${bold}Copy the Config Connector resources to one of clusters in each project.${normal}"
-#read -p ''
-#print_and_execute "cp -r k8s_manifests/prod/app-cnrm/* ../k8s-repo/${OPS_GKE_1_CLUSTER}/app-cnrm/"
-#print_and_execute "cp -r k8s_manifests/prod/app-cnrm/* ../k8s-repo/${DEV1_GKE_1_CLUSTER}/app-cnrm/"
-#print_and_execute "cp -r k8s_manifests/prod/app-cnrm/* ../k8s-repo/${DEV2_GKE_1_CLUSTER}/app-cnrm/"
-#
-#echo -e "\n"
-#echo "${bold}Replace the PROJECT_ID in the Config Connector manifests.${normal}"
-#read -p ''
-#print_and_execute "sed -i 's/\${PROJECT_ID}/'${TF_VAR_ops_project_name}'/g' ../k8s-repo/${OPS_GKE_1_CLUSTER}/app-cnrm/*"
-#print_and_execute "sed -i 's/\${PROJECT_ID}/'${TF_VAR_dev1_project_name}'/g'  ../k8s-repo/${DEV1_GKE_1_CLUSTER}/app-cnrm/*"
-#print_and_execute "sed -i 's/\${PROJECT_ID}/'${TF_VAR_dev2_project_name}'/g'  ../k8s-repo/${DEV2_GKE_1_CLUSTER}/app-cnrm/*"
-#
-#echo -e "\n"
-#echo "${bold}Copy loadgenerator manifests (Deployment, PodSecurityPolicy and RBAC) to the ops clusters.${normal}"
-#read -p ''
-#print_and_execute "cp -r k8s_manifests/prod/app-loadgenerator/. ../k8s-repo/gke-asm-1-r1-prod/app-loadgenerator/."
-#print_and_execute "cp -r k8s_manifests/prod/app-loadgenerator/. ../k8s-repo/gke-asm-2-r2-prod/app-loadgenerator/."
-#
-#echo -e "\n"
-#echo "${bold}Replace the ops project ID in the loadgenerator manifests for both ops clusters.${normal}"
-#read -p ''
-#print_and_execute "sed -i 's/OPS_PROJECT_ID/'${TF_VAR_ops_project_name}'/g'  ../k8s-repo/${OPS_GKE_1_CLUSTER}/app-loadgenerator/loadgenerator-deployment.yaml"
-#print_and_execute "sed -i 's/OPS_PROJECT_ID/'${TF_VAR_ops_project_name}'/g'  ../k8s-repo/${OPS_GKE_1_CLUSTER}/app-loadgenerator/loadgenerator-rbac.yaml"
-#print_and_execute "sed -i 's/OPS_PROJECT_ID/'${TF_VAR_ops_project_name}'/g'  ../k8s-repo/${OPS_GKE_2_CLUSTER}/app-loadgenerator/loadgenerator-deployment.yaml"
-#print_and_execute "sed -i 's/OPS_PROJECT_ID/'${TF_VAR_ops_project_name}'/g'  ../k8s-repo/${OPS_GKE_2_CLUSTER}/app-loadgenerator/loadgenerator-rbac.yaml"
-#
-#echo -e "\n"
-#echo "${bold}Add the loadgenerator resources to kustomization.yaml for both ops clusters.${normal}"
-#read -p ''
-#print_and_execute "cd ../k8s-repo/${OPS_GKE_1_CLUSTER}/app-loadgenerator/"
-#print_and_execute "kustomize edit add resource loadgenerator-psp.yaml"
-#print_and_execute "kustomize edit add resource loadgenerator-rbac.yaml"
-#print_and_execute "kustomize edit add resource loadgenerator-deployment.yaml"
-#print_and_execute "cd ../../${OPS_GKE_2_CLUSTER}/app-loadgenerator/"
-#print_and_execute "kustomize edit add resource loadgenerator-psp.yaml"
-#print_and_execute "kustomize edit add resource loadgenerator-rbac.yaml"
-#print_and_execute "kustomize edit add resource loadgenerator-deployment.yaml"
-#
+echo -e "\n"
+echo "${bold}Remove podsecuritypolicies, deployments and rbac directories from ops clusters kustomization.yaml${normal}"
+read -p ''
+print_and_execute "sed -i -e '/- deployments\//d' -e '/- podsecuritypolicies\//d'  -e '/- rbac\//d' ../k8s-repo/${OPS_GKE_1_CLUSTER}/app/kustomization.yaml"
+print_and_execute "sed -i -e '/- deployments\//d' -e '/- podsecuritypolicies\//d'  -e '/- rbac\//d' ../k8s-repo/${OPS_GKE_2_CLUSTER}/app/kustomization.yaml"
 
-#echo -e "\n"
-#echo "${bold}View changes to k8s-repo.${normal}"
-#read -p ''
-#print_and_execute "cd ${WORKDIR}/k8s-repo"
-#print_and_execute "git status"
-#
-#echo -e "\n"
-#echo "${bold}Commit to k8s-repo to trigger deployment.${normal}"
-#read -p ''
-#print_and_execute "git add . && git commit -am \"create app namespaces and install hipster shop\""
-#print_and_execute "git push"
+echo -e "\n"
+echo "${bold}Remove cartservice from all but one dev cluster ${normal}"
+read -p ''
+print_and_execute "rm ../k8s-repo/${DEV1_GKE_2_CLUSTER}/app/deployments/app-cart-service.yaml"
+print_and_execute "rm ../k8s-repo/${DEV1_GKE_2_CLUSTER}/app/podsecuritypolicies/cart-psp.yaml"
+print_and_execute "rm ../k8s-repo/${DEV1_GKE_2_CLUSTER}/app/rbac/cart-rbac.yaml"
+echo -e "\n"
+print_and_execute "rm ../k8s-repo/${DEV2_GKE_1_CLUSTER}/app/deployments/app-cart-service.yaml"
+print_and_execute "rm ../k8s-repo/${DEV2_GKE_1_CLUSTER}/app/podsecuritypolicies/cart-psp.yaml"
+print_and_execute "rm ../k8s-repo/${DEV2_GKE_1_CLUSTER}/app/rbac/cart-rbac.yaml"
+echo -e "\n"
+print_and_execute "rm ../k8s-repo/${DEV2_GKE_2_CLUSTER}/app/deployments/app-cart-service.yaml"
+print_and_execute "rm ../k8s-repo/${DEV2_GKE_2_CLUSTER}/app/podsecuritypolicies/cart-psp.yaml"
+print_and_execute "rm ../k8s-repo/${DEV2_GKE_2_CLUSTER}/app/rbac/cart-rbac.yaml"
+
+echo -e "\n"
+echo "${bold}Add cartservice deployment, rbac and podsecuritypolicy to kustomization.yaml in the first dev cluster only${normal}"
+read -p ''
+print_and_execute "cd ../k8s-repo/${DEV1_GKE_1_CLUSTER}/app"
+print_and_execute "cd deployments && kustomize edit add resource app-cart-service.yaml"
+print_and_execute "cd ../podsecuritypolicies && kustomize edit add resource cart-psp.yaml"
+print_and_execute "cd ../rbac && kustomize edit add resource cart-rbac.yaml"
+print_and_execute "cd ${WORKDIR}/asm"
+
+echo -e "\n"
+echo "${bold}Replace the PROJECT_ID in the RBAC manifests.${normal}"
+read -p ''
+print_and_execute "sed -i 's/\${PROJECT_ID}/'${TF_VAR_dev1_project_name}'/g'  ../k8s-repo/${DEV1_GKE_1_CLUSTER}/app/rbac/*"
+print_and_execute "sed -i 's/\${PROJECT_ID}/'${TF_VAR_dev1_project_name}'/g'  ../k8s-repo/${DEV1_GKE_2_CLUSTER}/app/rbac/*"
+print_and_execute "sed -i 's/\${PROJECT_ID}/'${TF_VAR_dev2_project_name}'/g'  ../k8s-repo/${DEV2_GKE_1_CLUSTER}/app/rbac/*"
+print_and_execute "sed -i 's/\${PROJECT_ID}/'${TF_VAR_dev2_project_name}'/g'  ../k8s-repo/${DEV2_GKE_2_CLUSTER}/app/rbac/*"
+
+echo -e "\n"
+echo "${bold}Copy the IngressGateway and VirtualService manifests to the source repo for the ops clusters.${normal}"
+read -p ''
+print_and_execute "cp -r k8s_manifests/prod/app-ingress/* ../k8s-repo/${OPS_GKE_1_CLUSTER}/app-ingress/"
+print_and_execute "cp -r k8s_manifests/prod/app-ingress/* ../k8s-repo/${OPS_GKE_2_CLUSTER}/app-ingress/"
+
+echo -e "\n"
+echo "${bold}Copy the Config Connector resources to one of clusters in each project.${normal}"
+read -p ''
+print_and_execute "cp -r k8s_manifests/prod/app-cnrm/* ../k8s-repo/${OPS_GKE_1_CLUSTER}/app-cnrm/"
+print_and_execute "cp -r k8s_manifests/prod/app-cnrm/* ../k8s-repo/${DEV1_GKE_1_CLUSTER}/app-cnrm/"
+print_and_execute "cp -r k8s_manifests/prod/app-cnrm/* ../k8s-repo/${DEV2_GKE_1_CLUSTER}/app-cnrm/"
+
+echo -e "\n"
+echo "${bold}Replace the PROJECT_ID in the Config Connector manifests.${normal}"
+read -p ''
+print_and_execute "sed -i 's/\${PROJECT_ID}/'${TF_VAR_ops_project_name}'/g' ../k8s-repo/${OPS_GKE_1_CLUSTER}/app-cnrm/*"
+print_and_execute "sed -i 's/\${PROJECT_ID}/'${TF_VAR_dev1_project_name}'/g'  ../k8s-repo/${DEV1_GKE_1_CLUSTER}/app-cnrm/*"
+print_and_execute "sed -i 's/\${PROJECT_ID}/'${TF_VAR_dev2_project_name}'/g'  ../k8s-repo/${DEV2_GKE_1_CLUSTER}/app-cnrm/*"
+
+echo -e "\n"
+echo "${bold}Copy loadgenerator manifests (Deployment, PodSecurityPolicy and RBAC) to the ops clusters.${normal}"
+read -p ''
+print_and_execute "cp -r k8s_manifests/prod/app-loadgenerator/. ../k8s-repo/gke-asm-1-r1-prod/app-loadgenerator/."
+print_and_execute "cp -r k8s_manifests/prod/app-loadgenerator/. ../k8s-repo/gke-asm-2-r2-prod/app-loadgenerator/."
+
+echo -e "\n"
+echo "${bold}Replace the ops project ID in the loadgenerator manifests for both ops clusters.${normal}"
+read -p ''
+print_and_execute "sed -i 's/OPS_PROJECT_ID/'${TF_VAR_ops_project_name}'/g'  ../k8s-repo/${OPS_GKE_1_CLUSTER}/app-loadgenerator/loadgenerator-deployment.yaml"
+print_and_execute "sed -i 's/OPS_PROJECT_ID/'${TF_VAR_ops_project_name}'/g'  ../k8s-repo/${OPS_GKE_1_CLUSTER}/app-loadgenerator/loadgenerator-rbac.yaml"
+print_and_execute "sed -i 's/OPS_PROJECT_ID/'${TF_VAR_ops_project_name}'/g'  ../k8s-repo/${OPS_GKE_2_CLUSTER}/app-loadgenerator/loadgenerator-deployment.yaml"
+print_and_execute "sed -i 's/OPS_PROJECT_ID/'${TF_VAR_ops_project_name}'/g'  ../k8s-repo/${OPS_GKE_2_CLUSTER}/app-loadgenerator/loadgenerator-rbac.yaml"
+
+echo -e "\n"
+echo "${bold}Add the loadgenerator resources to kustomization.yaml for both ops clusters.${normal}"
+read -p ''
+print_and_execute "cd ../k8s-repo/${OPS_GKE_1_CLUSTER}/app-loadgenerator/"
+print_and_execute "kustomize edit add resource loadgenerator-psp.yaml"
+print_and_execute "kustomize edit add resource loadgenerator-rbac.yaml"
+print_and_execute "kustomize edit add resource loadgenerator-deployment.yaml"
+print_and_execute "cd ../../${OPS_GKE_2_CLUSTER}/app-loadgenerator/"
+print_and_execute "kustomize edit add resource loadgenerator-psp.yaml"
+print_and_execute "kustomize edit add resource loadgenerator-rbac.yaml"
+print_and_execute "kustomize edit add resource loadgenerator-deployment.yaml"
+
+
+echo -e "\n"
+echo "${bold}View changes to k8s-repo.${normal}"
+read -p ''
+print_and_execute "cd ${WORKDIR}/k8s-repo"
+print_and_execute "git status"
+
+echo -e "\n"
+echo "${bold}Commit to k8s-repo to trigger deployment.${normal}"
+read -p ''
+print_and_execute "git add . && git commit -am \"create app namespaces and install hipster shop\""
+print_and_execute "git push"
 
 echo -e "\n"
 echo "${bold}Wait for Cloud Build to finish.${normal}"
