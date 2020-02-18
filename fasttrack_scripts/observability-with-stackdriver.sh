4b33590019bf8b0509e4d5208b70e027a3ea3772
Steve McGhee <smcghee@google.com>
2020-02-18 14:37:48 +0900

    fix newlines, etc

--- fasttrack_scripts/observability-with-stackdriver.sh
+++ fasttrack_scripts/observability-with-stackdriver.sh
@@ -76,22 +76,34 @@ print_and_execute "kubectl --context ${OPS_GKE_1} get handler -n istio-system"
 title_and_wait "Verify that the Istio metrics export to Stackdriver is working. Click the link output from this command:"
 echo "https://console.cloud.google.com/monitoring/metrics-explorer?cloudshell=false&project=${TF_VAR_ops_project_name}"
 
-title_and_wait "Now let's add our pre-canned metrics dashboard. \n We are going to be using the Dashboard API directly.\n This is something you wouldn't normally do by hand-generating API calls,\n it would be part of an automation system, or you would build the dashboard manually \n in the web UI. This will get us started quickly:"
+title_and_wait "Now let's add our pre-canned metrics dashboard. \
+    We are going to be using the Dashboard API directly.\
+    This is something you wouldn't normally do by hand-generating API calls,\
+    it would be part of an automation system, or you would build the dashboard manually \
+    in the web UI. This will get us started quickly:"
 print_and_execute "cd ${WORKDIR}/asm/k8s_manifests/prod/app-telemetry/"
 print_and_execute "sed -i 's/OPS_PROJECT/'${TF_VAR_ops_project_name}'/g'  services-dashboard.json"
 print_and_execute "OAUTH_TOKEN=$(gcloud auth application-default print-access-token)"
-print_and_execute "curl -X POST -H \"Authorization: Bearer $OAUTH_TOKEN\" -H \"Content-Type: application/json\" \\"
-print_and_execute "       https://monitoring.googleapis.com/v1/projects/${TF_VAR_ops_project_name}/dashboards \\"
-print_and_execute "       -d @services-dashboard.json "
+print_and_execute "curl -X POST -H \"Authorization: Bearer $OAUTH_TOKEN\" -H \"Content-Type: application/json\" \
+                        https://monitoring.googleapis.com/v1/projects/${TF_VAR_ops_project_name}/dashboards \
+                        -d @services-dashboard.json "
 
 title_and_wait "Navigate to the output link below to view the newly added dashboard."
 echo "https://console.cloud.google.com/monitoring/dashboards/custom/servicesdash?cloudshell=false&project=${TF_VAR_ops_project_name}"
  
-title_and_wait "We could edit the dashboard in-place using the UX, but in our case \n we are going to quickly add a new graph using the API.\n In order to do that, you should pull down the latest version\n of the dashboard, apply your edits, then push it back up using the HTTP PATCH method.\n You can get an existing dashboard by querying the monitoring API.\n Get the existing dashboard that was just added:"
+title_and_wait "We could edit the dashboard in-place using the UX, but in our case \
+    we are going to quickly add a new graph using the API.\
+    In order to do that, you should pull down the latest version\
+    of the dashboard, apply your edits, then push it back up using the HTTP PATCH method.\
+    You can get an existing dashboard by querying the monitoring API.\
+    Get the existing dashboard that was just added:"
 print_and_execute "curl -X GET -H \"Authorization: Bearer $OAUTH_TOKEN\" -H \"Content-Type: application/json\" \\"
 print_and_execute "       https://monitoring.googleapis.com/v1/projects/${TF_VAR_ops_project_name}/dashboards/servicesdash > sd-services-dashboard.json"
  
-title_and_wait "Add a new graph: (50th %ile latency): [API reference] Now we can add a new graph widget \n to our dashboard in code. This change can be reviewed by peers and checked into version control.\n Here is a widget to add that shows 50%ile latency (median latency).\n Try editing the dashboard you just got, adding a new stanza:"
+title_and_wait "Add a new graph: (50th %ile latency): [API reference] Now we can add a new graph widget \
+    to our dashboard in code. This change can be reviewed by peers and checked into version control.\
+    Here is a widget to add that shows 50%ile latency (median latency).\
+    Try editing the dashboard you just got, adding a new stanza:"
 print_and_execute "jq --argjson newChart \"$(<new-chart.json)\" '.gridLayout.widgets += [$newChart]' sd-services-dashboard.json > patched-services-dashboard.json"
  
 title_and_wait "Update the existing servicesdashboard:"
